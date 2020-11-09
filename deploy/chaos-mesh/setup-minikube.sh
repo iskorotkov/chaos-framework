48db5fb99cfde91114975079c9740acf5f6349ab
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-11-09 21:34:12 +0500

    fix: split Chaos Mesh setup script into versions for Kind and Minikube

---
+++ deploy/chaos-mesh/setup-minikube.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Add Chaos Mesh repository to Helm repos
+helm repo add chaos-mesh https://charts.chaos-mesh.org
+
+# Create custom resource type
+curl -sSL https://mirrors.chaos-mesh.org/v1.0.2/crd.yaml | kubectl apply -f -
+
+# Install Chaos Mesh
+kubectl create ns chaos-mesh
+helm install chaos-mesh chaos-mesh/chaos-mesh --namespace=chaos-mesh --set dashboard.create=true
