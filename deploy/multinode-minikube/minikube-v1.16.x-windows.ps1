f78d4a001a79bcc404fe6f0c67dd73e4bbb70e70
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-12-28 13:13:43 +0500

    feat: update scripts to support virtualbox

---
+++ deploy/multinode-minikube/minikube-v1.16.x-windows.ps1
@@ -0,0 +1,3 @@
+# Start 3 node cluster using VirtualBox driver and default Kubernetes version (v1.20.x at the moment of writing)
+# Note: skip VT-X check that prevents cluster from starting
+minikube start --driver=virtualbox --no-vtx-check --nodes=3
